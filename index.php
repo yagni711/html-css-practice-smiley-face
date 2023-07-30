2b3e78f003eeec7a8a661e259029a7c2139d79c0
Yagni Patel <140079410+yagni711@users.noreply.github.com>
2023-07-30 11:26:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Emoji</title>
+<link rel="icon" type="image/x-icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt4dirYFZGmEqJdbzkel2FcVL-YrjEvK8Cvg&usqp=CAU">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+<style>
+
+</style> 
+</head>
+<body>
+  <center>
+  <div class="smile">
+    <div class="eye1"></div>
+    <!-- <hr> -->
+    <div class="eye2"></div>
+    <!-- <hr> -->
+    <!-- <img src="https://clipartix.com/wp-content/uploads/2016/08/Toothy-smile-clipart.gif"> -->
+    <img src="smile-cartoon-simple-line-tom-hill-removebg-preview.png"> 
+
+  </div>
+  
+    <img src="https://static.vecteezy.com/system/resources/previews/012/422/197/original/circle-vintage-frame-black-line-art-ornate-patterned-frame-with-transparent-background-png.png" class="imgimg1"> 
+    <img src="https://images.vexels.com/media/users/3/323484/isolated/preview/550775b018f5a23e8a7ae00104b2121c-artistic-black-and-white-symmetric-mandala-shape.png" class="imgimg">
+
+       <img src="https://static.vecteezy.com/system/resources/previews/012/422/197/original/circle-vintage-frame-black-line-art-ornate-patterned-frame-with-transparent-background-png.png"  class="imgimg3"> 
+    <img src="https://images.vexels.com/media/users/3/323484/isolated/preview/550775b018f5a23e8a7ae00104b2121c-artistic-black-and-white-symmetric-mandala-shape.png" class="imgimg2">
+
+
+      <!-- <img src="https://static.vecteezy.com/system/resources/previews/012/422/197/original/circle-vintage-frame-black-line-art-ornate-patterned-frame-with-transparent-background-png.png" style="width:50px;height:50px;position: absolute;top:150px;left:320px;" > 
+    <img src="https://images.vexels.com/media/users/3/323484/isolated/preview/550775b018f5a23e8a7ae00104b2121c-artistic-black-and-white-symmetric-mandala-shape.png"> -->
+
+
+</center>
+</body>
+</html>
+
